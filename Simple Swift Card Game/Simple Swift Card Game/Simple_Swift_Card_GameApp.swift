36f80a8b4fbf392d542d892356e192b3a699f007
Stephen <77303477+StephenCarras@users.noreply.github.com>
2023-09-26 20:55:32 -0700

    Added files
    Files include images and Swift Code and Xcode file.

---
+++ Simple Swift Card Game/Simple Swift Card Game/Simple_Swift_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Swift_Card_GameApp.swift
+//  Simple Swift Card Game
+//
+//  Created by Stephen Carras on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Swift_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
